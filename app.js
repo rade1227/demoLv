c8e1d3053307c16f6b485a2c9b4dec450371f6ff
rade1227 <90378016+rade1227@users.noreply.github.com>
2024-03-19 19:47:00 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// MENU DROPDOWN
+
+let nav = document.querySelector('nav');
+let dropdown = nav.querySelector('.dropdown');
+let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let navToggle = nav.querySelector("[data-action='nav-toggle']");
+
+dropdownToggle.addEventListener('click', () => {
+	if (dropdown.classList.contains('show')) {
+		dropdown.classList.remove('show');
+	} else {
+		dropdown.classList.add('show');
+	}
+})
+
+navToggle.addEventListener('click', () => {
+	if (nav.classList.contains('opened')) {
+		nav.classList.remove('opened');
+	} else {
+		nav.classList.add('opened');
+	}
+});
+
+
+// (document).ready(function() {
+
+// 	(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function() {
+// 	  $(this).parent().children(".overlay").show();
+  
+// 	});
+  
+  
+// 	(`[unique-script-id="w-w-dm-id"] .closegal`).click(function() {
+// 	  $(`[unique-script-id="w-w-dm-id"] .overlay`).hide();
+// 	});
+//   });
+
+  
